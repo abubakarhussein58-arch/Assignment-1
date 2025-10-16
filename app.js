9dfe9c3f66e1ce99bc72be9e16178cac1988ae8f
abubakarhussein58-arch <abubakarhussein58@gmail.com>
2025-10-16 09:26:09 +0200

    Assignment 1

---
+++ app.js
@@ -0,0 +1,8 @@
+// Hitta knappen och rubriken i HTML
+const button = document.getElementById('colorButton');
+const title = document.getElementById('title');
+
+// När knappen klickas, ändra färgen på rubriken
+button.addEventListener('click', function() {
+    title.style.color = title.style.color === 'blue' ? 'green' : 'blue';
+});
